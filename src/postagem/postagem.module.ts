079120cce00ef0ed15dbe6b21bd1a25f96e3c957
ysabellax <ysabella3909@gmail.com>
2025-06-23 13:54:49 -0300

    Iniciando projeto utilizando NestJs

---
+++ src/postagem/postagem.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postagem } from './entities/postagem.entity';
+import { PostagemController } from './controllers/postagem.controller';
+import { PostagemService } from './services/postagem.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Postagem])],
+  providers: [PostagemService],
+  controllers: [PostagemController],
+  exports: [],
+})
+export class PostagemModule {}
